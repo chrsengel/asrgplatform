f03d75791d30ff9b7899ceaf4da09f1431d9000e
Christian Engel <engel.christian1994@gmail.com>
2020-04-26 22:42:10 +0200

    Set up tailwind config

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      body: ["Roboto", "sans-serif"],
+      display: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        darkBlue: "#0e2439",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
